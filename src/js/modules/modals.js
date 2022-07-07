31d03d9c4bf5cefd1faa484397551591b015440c
gapur <kaloycant@gmail.com>
2022-07-07 23:50:11 +0300

    Fix last

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,27 +1,30 @@
 const modals = () => {
   function bindModal(triggerSelector, modalSelector) {
     let triggers = document.querySelectorAll(triggerSelector),
-        modal = document.querySelector(modalSelector);
-      
+      modal = document.querySelector(modalSelector);
+
     function showModal(modal) {
-      document.body.classList.add('modal-open');
+      document.body.classList.add("modal-open");
       modal.classList.add("show");
       modal.classList.remove("hide");
     }
-    
+
     function hideModal(modal) {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove("modal-open");
       modal.classList.remove("show");
       modal.classList.add("hide");
     }
 
-    triggers.forEach(item => {
-      item.addEventListener('click', () => {
+    triggers.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
         showModal(modal);
       });
     });
 
-    modal.addEventListener('click', (e) => {
+    modal.addEventListener("click", (e) => {
       if (e.target === modal || e.target.classList.contains("popup_close")) {
         hideModal(modal);
       }
@@ -30,7 +33,7 @@ const modals = () => {
 
   function showModalByTime(selector, time) {
     setTimeout(() => {
-      document.body.classList.add('modal-open');
+      document.body.classList.add("modal-open");
       document.querySelector(selector).classList.add("show");
       document.querySelector(selector).classList.remove("hide");
     }, time);
