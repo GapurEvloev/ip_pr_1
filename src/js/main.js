3691f14ddc61a6c4a98d81db3e69ab2571f391cc
gapur <kaloycant@gmail.com>
2022-07-18 16:33:02 +0200

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -16,4 +17,5 @@ document.addEventListener("DOMContentLoaded", () => {
   tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
   tabs(".balcon_icons", ".balcon_icons_img", ".big_img > img", "do_image_more");
   forms(modalState);
+  timer("#timer", "2022-08-13");
 });
